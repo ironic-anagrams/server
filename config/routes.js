9698545576fccbba1fdaea260643311e396bdd2e
Tim Z <tjkz87@gmail.com>
2016-09-05 12:45:08 -0700

    Added authentication. /api/signup, /api/signin, and x-access-token header

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,15 @@
 var entryController = require('../controllers/entryController.js');
 var userController = require('../controllers/userController.js');
+var utils = require('./utils.js');
 
 module.exports = function(app, express) {
+  app.post('/api/signup', userController.createUser);
+  app.post('/api/signin', userController.signIn);
+
+  app.use('/api/entries', utils.decode);
   app.post('/api/entries', entryController.createEntry);
   app.get('/api/entries', entryController.getEntries);
-  app.post('/api/users', userController.createUser);
+  
+  app.use('/api/users', utils.decode);
   app.get('/api/users', userController.findUser);
-
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 var db = require('../models/Database.js');
+var jwt = require('jwt-simple');
 
 module.exports = {
 
@@ -8,8 +9,10 @@ module.exports = {
       .then(function(newUser){
         console.log("Creating new User: ", newUser);
         //add error handling logic
+        var token = jwt.encode(newUser, 'secret');
         res.status(201).json({
-          id: newUser.id
+          // id: newUser.id,
+          token: token
         })
       })
       .catch(function(err){
@@ -30,7 +33,54 @@ module.exports = {
       .catch(function(err){
         res.status(404).json(err)
       })
-  }
+  },
 
+  signIn: function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    db.User.findOne({ where: {username: username}} )
+     .then(function (user) {
+        if (!user) {
+          //next(new Error('User does not exist'));
+          res.status(404).json({ error: 'User does not exist' })
+        } else {
+            if (password === user.password){
+              var token = jwt.encode(user, 'secret');
+              res.json({token: token});
+            } else {
+                //next(new Error('Incorrect password'));
+                res.status(401).json({error: 'Incorrect password'})
+            }
+        }
+      })
+      .catch(function(err){
+        res.json(err);
+      })
+  },
+  
+  checkAuth: function(req, res, next) {
+    // checking to see if the user is authenticated
+    // grab the token in the header is any
+    // then decode the token, which we end up being the user object
+    // check to see if that user exists in the database
+    var token = req.headers['x-access-token'];
+    if (!token) {
+      //next(new Error('No token'));
+      res.status(401).json({ error: 'No token' });
+    } else {
+      var user = jwt.decode(token, 'secret');
+      db.User.findOne({ where: {username: user.username}} )
+        .then(function (user) {
+          if (user) {
+            res.send(200);
+          } else {
+            res.send(401);
+          }
+        })
+        .catch(function (error) {
+          next(error);
+        });
+    }
+  }
 
 };
