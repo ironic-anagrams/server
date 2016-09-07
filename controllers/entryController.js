e433fbcd37d0dd5019b6fa66ffdf3dc13470a651
Tim Z <tjkz87@gmail.com>
2016-09-07 10:10:09 -0700

    get /entries return only own or friends entries

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -16,16 +16,42 @@ module.exports = {
   },
 
   getEntries: function(req, res, next) {
-    // TODO: check if req.query.userId is in friendlist
-    var userId = req.query.userId || req.user.id;
-
-    db.Entry.findAll({ where: { userId: userId }})
+    
+    //var userId = req.query.userId || req.user.id;
+    console.log('req.query.userId:', req.query.userId);
+    console.log('req.user.id:', req.user.id);
+    if (req.query.userId && (req.query.userId !== req.user.id.toString())) {
+      // check if req.query.userId is in friendlist
+      db.Relationships.findOne({ 
+        where: { user1: req.user.id, user2: req.query.userId }
+      })
+        .then(function(friends) {
+          console.log('friends', friends);
+          if (friends) {
+            // send entries
+            db.Entry.findAll({ where: { userId: req.query.userId }})
+              .then(function(entries){
+                res.send(entries);
+              })
+              .catch(function(err){
+                res.status(404).json(err)
+              });
+          } else {
+            res.status(404).json({ error: 'you are not friends'})
+          }
+        })
+        .catch(function(err) {
+          res.status(404).json(err)
+        });
+    } else {
+      db.Entry.findAll({ where: { userId: req.user.id }})
       .then(function(entries){
         res.send(entries);
       })
       .catch(function(err){
         res.status(404).json(err)
       });
+    }
   }
 
 };
