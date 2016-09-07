b8adb154252cac41d4041b817936d05bad863000
Tim Z <tjkz87@gmail.com>
2016-09-06 18:58:58 -0700

    get /friendreq returns a list of incoming friend requests

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -16,29 +16,42 @@ module.exports = {
   },
 
   getRequests: function(req, res, next){
-    db.Request.findAll({where: {requestReceiver: req.user.id}})
-      .then(function(requestList){
-        var query = requestList.reduce(function(total,request){
-          total.push(request.dataValues.userId)
-          return total;
-        },[])
-        db.User.findAll({
-          where: {
-              id: {
-                $any: query
-              }
-          }
-        })
-          .then(function(requests){
-            console.log("This is the friendslist", requests)
-            res.status(201).json(requests)
-          })
-          .catch(function(err){
-            res.status(404).json(err)
-          })
+    // db.Request.findAll({where: {requestReceiver: req.user.id}})
+    //   .then(function(requestList){
+    //     var query = requestList.reduce(function(total,request){
+    //       total.push(request.dataValues.userId)
+    //       return total;
+    //     },[])
+    //     db.User.findAll({
+    //       where: {
+    //           id: {
+    //             $any: query
+    //           }
+    //       }
+    //     })
+    //       .then(function(requests){
+    //         console.log("This is the friendslist", requests)
+    //         res.status(201).json(requests)
+    //       })
+    //       .catch(function(err){
+    //         res.status(404).json(err)
+    //       })
+    //   })
+    //   .catch(function(err){
+    //     res.status(404).json(err)
+    //   })
+    db.Request.findAll({
+      where: { requestReceiver: req.user.id },
+      include: {
+        model: db.User,
+        attributes: ['fullname']
+      }
+    })
+      .then(function(requestList) {
+        res.status(200).json(requestList);
       })
-      .catch(function(err){
-        res.status(404).json(err)
-      })
+      .catch(function(err) {
+        res.status(404).json(err);
+      });
   }
 }
